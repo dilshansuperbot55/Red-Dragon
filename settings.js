32378f116ffefde1f4d92cadb471ace8912cf769
dilshansuperbot55 <115202602+dilshansuperbot55@users.noreply.github.com>
2022-10-07 09:16:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
 global.vcardowner = ['+94750508470'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.ownername = "🆆🅼🅷 ᴅɪʟꜱʜᴀɴ ꜱᴜʀᴀᴡᴇᴇʀᴀ ✌️" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
